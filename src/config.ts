697c23e66a27303d0274b72f25fce7b546c2a7df
laitman <47023817+LaitmanX@users.noreply.github.com>
2024-11-25 01:42:18 +0100

    refactor

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,9 @@
 export const CONFIG = {
   TELEGRAM_API_URL: `https://api.telegram.org/bot${ENV_BOT_TOKEN}/`,
   WELCOME_IMAGE_URL:
-    "https://cdn.midjourney.com/b2782367-5cec-4bfb-bc55-31f7a00fb8b1/0_3.png",
-  ALPHA_APP_URL: "https://app.alpha.broker",
+    "https://raw.githubusercontent.com/ALPHA-BROKER/alpha-content/refs/heads/main/alpha.gif",
+  SERVER_URL: "https://app.alpha.broker/api/auth/login",
+  ALPHA_APP_URL: "https://app.alpha.broker/",
   ALPHA_SOCIAL_URL: "https://t.me/alphabroker_x",
   ALPHA_TELEGRAM_URL: "https://t.me/alphabroker_chat",
   ALPHA_AIRDROP_URL: "https://t.me/getalphaapp",
--- src/handlers/handleStartCommand.ts
+++ src/handlers/handleStartCommand.ts
@@ -1,18 +1,49 @@
-import { sendTelegramPhoto } from "./sendTelegramPhoto";
-import { localizations } from "../localization/localization";
+import { sendTelegramMessage } from "./sendTelegramMessage";
 import { CONFIG } from "../config";
 
 export async function handleStartCommand(
   chatId: number,
-  languageCode: string
+  username: string | undefined
 ): Promise<void> {
-  const userLang = localizations[languageCode] ? languageCode : "en";
-  const { welcomeText, buttons } = localizations[userLang];
+  try {
+    // Обращаемся к API для аутентификации
+    const response = await fetch(`https://app.alpha.broker/api/auth/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        telegramId: chatId,
+        username: username || "unknown",
+        code: "123456",
+      }),
+    });
 
-  await sendTelegramPhoto(
-    chatId,
-    CONFIG.WELCOME_IMAGE_URL,
-    welcomeText,
-    buttons
-  );
+    if (!response.ok) {
+      const errorText = await response.text();
+      console.error("Error authenticating user:", errorText);
+      throw new Error(`Error authenticating user: ${errorText}`);
+    }
+
+    const { link } = await response.json();
+
+    // Текст и кнопка для пользователя
+    const welcomeText = `
+      🚀 Welcome to ALPHA BROKER!
+      
+      🔑 Manage wallets and launch tokens on Solana.
+      👇 Click the button below to start your journey:
+    `;
+
+    const buttons = [[{ text: "Go to ALPHA BROKER", url: link }]];
+
+    // Отправляем сообщение с кнопкой
+    await sendTelegramMessage(chatId, welcomeText, buttons);
+  } catch (error) {
+    console.error("Error handling /start command:", error);
+    await sendTelegramMessage(
+      chatId,
+      "An error occurred while processing your request. Please try again later."
+    );
+  }
 }
